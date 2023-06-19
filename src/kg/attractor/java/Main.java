90c7efe478230d98a738daed1290d4af66f08e0f
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-06-19 23:41:57 +0600

    Added some warnings

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        //не менять порт!так как в html файле есть фрагмент <a href="http://localhost:8187/employee" class="booklink">
+        //который переключает на другую страницу
         try {
             new Lesson44Server("localhost", 8187).start();
         } catch (IOException e) {
